52f4ae91637073dfb1bfe09bf7cd9c147cd2e60b
DevSecOps-Amit <amitsingh.6june@gmail.com>
2025-08-21 19:58:58 +0530

    Azure-Infra-Code-Terraform

---
+++ Child-Module/azurerm_key_vault/variables.tf
@@ -0,0 +1,7 @@
+variable "key_vault_name" {
+  type        = string
+  description = "Key Vault ka Naam"
+  default     = "infra-app-vault"
+}
+variable "location" {}
+variable "resource_group_name" {}
